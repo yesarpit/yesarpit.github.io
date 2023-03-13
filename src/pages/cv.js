4b5f9e349c01cd3871ad4e2736a4deccf35b17e5
Arpit Sharma <yesarpit@gmail.com>
2023-03-14 00:50:58 +0530

    dummy commit

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '@theme/Layout';
 export default function CV() {
     return <Layout
-        title={`Arpit's CV`}
+        title={`Arpit Sharma's CV`}
         description="Arpit Sharma's CV">
         <main style={{ height: "100vh" }}>
             <object data={require('@site/static/img/CV.pdf').default} type="application/pdf" style={{ height: "-webkit-fill-available", width: " -webkit-fill-available" }} />
